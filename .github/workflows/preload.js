d19c7ff2a09845f2a6bf746761b9287a203eba7e
johanmadajmjm-beep <johanmada.jm.jm@gmail.com>
2026-05-24 13:19:50 +0800

    Delete .github/workflows/preload.js

--- .github/workflows/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  minimize: () => ipcRenderer.send('win-minimize'),
-  maximize: () => ipcRenderer.send('win-maximize'),
-  close: () => ipcRenderer.send('win-close')
-})
